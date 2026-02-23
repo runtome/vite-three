7eb9e808adda4baef92fcd9502427ac8b9a04858
SUPHOT NARAPONG <suphot.n@gmail.com>
2026-02-23 14:53:44 +0700

    Scene

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+import { GUI } from 'dat.gui'
 
-const scene = new THREE.Scene()
+const sceneA = new THREE.Scene()
+sceneA.background = new THREE.Color(0x123456)
+
+const sceneB = new THREE.Scene()
+sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
+
+const sceneC = new THREE.Scene()
+sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+// sceneC.backgroundBlurriness = 0.5
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
@@ -20,17 +28,34 @@ window.addEventListener('resize', () => {
 })
 
 new OrbitControls(camera, renderer.domElement)
+
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+sceneA.add(cube)
+// sceneC.add(cube)
 
-const stast = new Stats()
-// stast.showPanel(1)
-document.body.appendChild(stast.dom)
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+let activeScene = sceneA
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA
+  },
+  sceneB: () => {
+    activeScene = sceneB
+  },
+  sceneC: () => {
+    activeScene = sceneC
+  },
+}
 
 const gui = new GUI()
+gui.add(setScene, 'sceneA').name('Scene A')
+gui.add(setScene, 'sceneB').name('Scene B')
+gui.add(setScene, 'sceneC').name('Scene C')
 
 const cubeFolder = gui.addFolder('Cube')
 cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
@@ -45,13 +70,12 @@ cameraFolder.open()
 function animate() {
   requestAnimationFrame(animate)
 
-  // cube.rotation.x += 0.01
-  // stast.begin()
-  // cube.rotation.y += 0.01
-  // stast.end()
+  //cube.rotation.x += 0.01
+  //cube.rotation.y += 0.01
 
-  renderer.render(scene, camera)
-  stast.update()
+  renderer.render(activeScene, camera)
+
+  stats.update()
 }
 
 animate()
